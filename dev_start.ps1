7ded16a1fa8a5b49fb2189cf52224701704f6023
phylroy <phylroy.lopez@gmail.com>
2016-10-24 23:01:02 -0400

    removed prompt

--- dev_start.ps1
+++ dev_start.ps1
@@ -3,5 +3,4 @@ $current_user=[Environment]::UserName
 $ipconfig = $(ipconfig | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' } | select -First 1 | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' } | foreach{$Matches[1]} )
 Write-Host $ipconfig
 Write-Host $current_user
-docker start btap_dev 
-Read-Host -Prompt "Press Enter to exit"
+docker start btap_dev
