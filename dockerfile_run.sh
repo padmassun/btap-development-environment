6f5dc16fa8b242c8bb51da3eeb4b3bdb4ada6bad
phylroy <phylroy.lopez@gmail.com>
2016-10-26 12:13:32 -0400

    rename files

--- dockerfile_run.sh
+++ dockerfile_run.sh
@@ -2,7 +2,7 @@
 linux_home_folder=/root
 this_user=$(whoami)
 x_display=$(ipconfig | grep -m 1 "IPv4" | awk '{print $NF}')
-container_name= dockerfile_btap_dev
+container_name=dockerfile_btap_dev
 echo $this_user
 echo $x_display
 echo docker run -ti -e DISPLAY=$x_display:0.0 -v/c/Users/$this_user:$linux_home_folder/windows-host $container_name
