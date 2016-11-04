b9ffd1687533794198821da0f4b4cb6681d91e4c
phylroy <phylroy.lopez@gmail.com>
2016-11-04 16:45:44 -0400

    added argument to create container name.

--- dockerfile_create_container.sh
+++ dockerfile_create_container.sh
@@ -2,11 +2,11 @@
 linux_home_folder=/home/nrcan
 x_display=$(ipconfig | grep -m 1 "IPv4" | awk '{print $NF}')
 win_user=$(whoami)
-container=dockerfile_btap_dev_container
+container=$1
 image=dockerfile_btap_dev_image
 
 echo Windows users $win_user was detected.
-echo using  X server at this IP $x_display:0.0 . 
+echo using  X server at this IP $x_display:0.0 .
 echo docker create -ti -e DISPLAY=$x_display:0.0 -v/c/Users/$win_user:$linux_home_folder/windows-host --name $container $image
 docker rm $container
 docker create -ti -e DISPLAY=$x_display:0.0 -v/c/Users/$win_user:$linux_home_folder/windows-host --name $container $image
