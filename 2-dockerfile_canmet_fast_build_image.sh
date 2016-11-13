1a92f80d86a8ba9077ac5f1292321fba61c18fe1
phylroy <phylroy.lopez@gmail.com>
2016-11-12 23:12:06 -0500

    fixed script so gitbash can now be used for scripts on toolbax and docker for windows.

--- 2-dockerfile_canmet_fast_build_image.sh
+++ 2-dockerfile_canmet_fast_build_image.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-#Check if X server is running. 
-if [ "`ps | grep Xming`" == "" ]
-then
-#If not running try to start it. 
-if [ -d "/c/Program Files/Xming/" ]
-then
-  /c/Program\ Files/Xming/Xming.exe -ac -multiwindow -clipboard  -dpi 108 &
-elif [ -d "/c/Program Files (x86)/Xming/" ]
-then
-  /c/Program\ Files\ \(x86\)/Xming/Xming.exe -ac -multiwindow -clipboard  -dpi 108 &
-else
-	echo "Could not find Xming installed on your system either /c/Program Files/Xming or /c/Program Files (x86)/Xming  . Please install Xming, ideally the donation version in the default location." 
-	exit
-fi
-fi
+source .env.sh
 
 docker load -i //s-bcc-nas2/Groups/Common\ Projects/HB/dockerhub_images/btap-DE.img
 
--- 4-dockerfile_start_container.sh
+++ 4-dockerfile_start_container.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
-#Check if X server is running. 
-if [ "`ps | grep Xming`" == "" ]
-then
-#If not running try to start it. 
-if [ -d "/c/Program Files/Xming/" ]
-then
-  /c/Program\ Files/Xming/Xming.exe -ac -multiwindow -clipboard  -dpi 108 &
-elif [ -d "/c/Program Files (x86)/Xming/" ]
-then
-  /c/Program\ Files\ \(x86\)/Xming/Xming.exe -ac -multiwindow -clipboard  -dpi 108 &
-else
-	echo "Could not find Xming installed on your system either /c/Program Files/Xming or /c/Program Files (x86)/Xming  . Please install Xming, ideally the donation version in the default location." 
-	exit
-fi
-fi
-
-linux_home_folder=/home/nrcan
-x_display=$(ipconfig | grep -m 1 "IPv4" | awk '{print $NF}')
-win_user=$(whoami)
-container=$1
-image=dockerfile_btap_dev_image
-
+source ./env.sh
 echo Windows users $win_user was detected.
 echo using  X server at this IP $x_display:0.0 .
 echo start $1
