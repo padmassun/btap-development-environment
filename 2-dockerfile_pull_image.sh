1855c993c3fe6a6fc9b933a01ee447d703e04f84
phylroy <phylroy.lopez@gmail.com>
2017-09-26 23:08:03 -0400

    added pull script

---
+++ 2-dockerfile_pull_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source ./env.sh
+
+docker pull $image
+if test -d //s-bcc-nas2/Groups/Common\ Projects/HB/dockerhub_images/ ;
+then
+    echo "Found Canmet folder."
+    echo "Saving Image to Canmet folder $canmet_server_folder"
+	docker save -o //s-bcc-nas2/Groups/Common\ Projects/HB/dockerhub_images/$image.img $image
+fi
+
