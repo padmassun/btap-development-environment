61d37161bb01d5b5665b4d05342023e67f5d0179
Phylroy Lopez <phylroy.lopez@gmail.com>
2018-06-24 13:15:08 -0400

    bump to 2.4.3

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-os_version=2.4.1
+os_version=2.4.3
 image=canmet/btap-development-environment:$os_version
 canmet_server_folder=//s-bcc-nas2/Groups/Common\ Projects/HB/dockerhub_images/
 x_folder=nothing
